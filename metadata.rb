9ae570fd33376ea178040a053f3a18d63217d16e
Mike van Goor <mvangoor@schubergphilis.com>
2023-03-28 16:11:59 +0200

    Push patch

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'DMS Team'
 maintainer_email 'int-dms@schubergphilis.com'
 license          'Apache-2.0'
 description      'Installs and Configured CommVault client software'
-version          '1.0.0'
+version          '1.0.1'
 chef_version     '>= 14.0'
 
 issues_url 'https://github.com/sbp-cookbooks/commvault/issues'
